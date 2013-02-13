eca2359b374f53bddd16423a523e02ca985e3a23
Cyrus Omar <cyology@gmail.com>
2013-02-13 18:11:18 -0500

    Added distutils stuff

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+	name='cypy',
+	version='0.1.0',
+	author='Cyrus Omar',
+	author_email='cyrus.omar@gmail.com',
+        packages=['cypy', 'cypy.np'],
+	py_modules=['cypy', 'cypy.cg', 'cypy.astx', 'cypy.np', 'cypy.np.plotting'],
+	url='http://github.com/cyrus-/cypy',
+	license='MIT',
+	description='Useful utilities for Python.',
+	long_description="",
+	install_requires=[]
+)
