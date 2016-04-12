04bb59e91fa314e8cf987743189c77a9b6bc371d
Cyrus Omar <cyology@gmail.com>
2016-04-12 03:01:36 -0400

    upped version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
 	name='cypy',
-	version='0.1.0',
+	version='0.2.0',
 	author='Cyrus Omar',
 	author_email='cyrus.omar@gmail.com',
         packages=['cypy', 'cypy.np'],
